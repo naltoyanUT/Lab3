94161ae587fe311dbd922f52fee439aec6af4083
naltoyanUT <naltoyan@utexas.edu>
2015-11-10 17:02:14 -0600

    test 1 disabled

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -3,9 +3,9 @@ import Experiment
 
 class MyTestCase(unittest.TestCase):
 
-  def test_t1(self):
-    r1 = Experiment.largest([1,1,1]) 
-    self.assertEqual(r1, 1)
+  # def test_t1(self):
+  #   r1 = Experiment.largest([1,1,1])
+  #   self.assertEqual(r1, 1)
 
   def test_t2(self):
     r1 = Experiment.largest([3,2,1]) 
