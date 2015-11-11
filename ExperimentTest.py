75ff6b78d984912cbb4ecbdc089785cf9743a64e
naltoyanUT <naltoyan@utexas.edu>
2015-11-11 09:09:02 -0600

    enable all

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -7,14 +7,14 @@ class MyTestCase(unittest.TestCase):
     r1 = Experiment.largest([1,1,1])
     self.assertEqual(r1, 1)
 
-  # def test_t2(self):
-  #   r1 = Experiment.largest([3,2,1])
-  #   self.assertEqual(r1, 3)
-  #
-  # def test_t3(self):
-  #   r1 = Experiment.largest([3,2,3])
-  #   self.assertEqual(r1, 3)
-  #
+  def test_t2(self):
+    r1 = Experiment.largest([3,2,1])
+    self.assertEqual(r1, 3)
+
+  def test_t3(self):
+    r1 = Experiment.largest([3,2,3])
+    self.assertEqual(r1, 3)
+
   def test_t4(self):
     r1 = Experiment.largest([3,2,3,4])
     self.assertEqual(r1, 4)
@@ -27,8 +27,8 @@ class MyTestCase(unittest.TestCase):
     r1 = Experiment.largest([-3,-2,-3,-1,-4])
     self.assertEqual(r1, -1)
 
-  # def test_t7(self):
-  #  self.assertRaises(ValueError, Experiment.largest, [])
+  def test_t7(self):
+   self.assertRaises(ValueError, Experiment.largest, [])
 
 if __name__ == '__main__':
   unittest.main()
